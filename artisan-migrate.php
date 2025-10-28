ce68ce91bf35b2ea269b9211886a3384559b74e1
Hitendra Kakadiya <79523092+HitendraKakadiya@users.noreply.github.com>
2025-10-28 16:11:11 +0530

    fix session driver

--- artisan-migrate.php
+++ artisan-migrate.php
@@ -3,6 +3,11 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 echo "<pre>Running migrations...\n";
 
+// Ensure sessions do not use the database when running via web
+putenv('SESSION_DRIVER=file');
+$_ENV['SESSION_DRIVER'] = 'file';
+$_SERVER['SESSION_DRIVER'] = 'file';
+
 require __DIR__.'/vendor/autoload.php';
 $app = require_once __DIR__.'/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
